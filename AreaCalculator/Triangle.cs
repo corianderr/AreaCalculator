fa5e4212acbf4ec1159a270e55e9683a8bd0eb5e
cariander <carianderrr@gmail.com>
2023-02-01 18:45:27 +0600

    Add description of methods and properties

--- AreaCalculator/Triangle.cs
+++ AreaCalculator/Triangle.cs
@@ -15,7 +15,9 @@ public class Triangle : Figure
     /// Third side of a triangle.
     /// </summary>
     private double ThirdSide { get; }
-
+    /// <summary>
+    /// Perimeter of a triangle.
+    /// </summary>
     public double Perimeter
     {
         get
@@ -23,36 +25,59 @@ public class Triangle : Figure
             if (_perimeter == 0) _perimeter = CalculatePerimeter();
             return _perimeter;
         }
-        set => _perimeter = value;
+        init => _perimeter = value;
     }
+    /// <summary>
+    /// Perimeter of a triangle.
+    /// </summary>
+    public bool IsRightAngled { get; }
     
+    /// <summary>
+    /// Represents a figure with three corners (triangle) and provides option to calculate and use its area..
+    /// </summary>
+    /// <param name="firstSide">First side of a triangle.</param>
+    /// <param name="secondSide">Second side of a triangle.</param>
+    /// <param name="thirdSide">Third side of a triangle.</param>
+    /// <exception cref="ArgumentOutOfRangeException">If any of the sides is less than zero.</exception>
     public Triangle(double firstSide, double secondSide, double thirdSide)
     {
         if (firstSide <= 0) 
             throw new ArgumentOutOfRangeException(nameof(firstSide),"Side of a triangle must be positive.");
+        if (secondSide <= 0) 
+            throw new ArgumentOutOfRangeException(nameof(secondSide),"Side of a triangle must be positive.");
+        if (thirdSide <= 0) 
+            throw new ArgumentOutOfRangeException(nameof(thirdSide),"Side of a triangle must be positive.");
 
         FirstSide = firstSide;
         SecondSide = secondSide;
         ThirdSide = thirdSide;
         Perimeter = firstSide + secondSide + thirdSide;
+        IsRightAngled = CheckIsRightAngled();
     }
-
+    
     protected sealed override double CalculateArea()
     {
         return Math.Sqrt(Perimeter * (Perimeter - FirstSide) * (Perimeter - SecondSide) * (Perimeter - ThirdSide));
     }
 
+    /// <summary>
+    /// Calculates perimeter of a triangle.
+    /// </summary>
+    /// <returns>Floating point value of perimeter.</returns>
     private double CalculatePerimeter()
     {
         return FirstSide + SecondSide + ThirdSide;
     }
 
-    private bool IsRightAngled()
+    /// <summary>
+    /// Checks whether triangle is right-angled.
+    /// </summary>
+    /// <returns>True if triangle is right-angled, otherwise false.</returns>
+    private bool CheckIsRightAngled()
     {
         var maxSide = new[] { FirstSide, SecondSide, ThirdSide }.Max();
         var maxSquare = maxSide * maxSide;
         var sqrSum = FirstSide * FirstSide + SecondSide * SecondSide + ThirdSide * ThirdSide;
         return Math.Abs(maxSquare * 2 - sqrSum) < 0.00000001;
-        
     }
 }
