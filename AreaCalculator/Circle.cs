785dca779569a5ff3d56ccf5e00b4f6669efa143
cariander <carianderrr@gmail.com>
2023-02-01 17:02:19 +0600

    Create Circle class that inherits Figure class and implemented appropriate logic

---
+++ AreaCalculator/Circle.cs
@@ -0,0 +1,26 @@
+namespace AreaCalculator;
+
+public class Circle : Figure
+{
+    /// <summary>
+    /// Radius of circle.
+    /// </summary>
+    private double Radius { get; }
+    
+    /// <summary>
+    /// Represents a figure without corners (circle) and provides option to calculate and use its area.
+    /// </summary>
+    /// <param name="radius">Radius of circle.</param>
+    /// <exception cref="ArgumentOutOfRangeException">If radius is less than zero.</exception>
+    public Circle(double radius)
+    {
+        if (radius < 0) 
+            throw new ArgumentOutOfRangeException(nameof(radius),"Radius cannot be negative.");
+        Radius = radius;
+    }
+
+    protected sealed override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
